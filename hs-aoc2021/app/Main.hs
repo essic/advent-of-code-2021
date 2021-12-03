fa9177c37f50bb4a983e36d8426f15fe7408db04
essic <lollancf37@gmail.com>
2021-12-03 20:40:27 +0100

    chore: solve day 1.

--- hs-aoc2021/app/Main.hs
+++ hs-aoc2021/app/Main.hs
@@ -1,7 +1,36 @@
+{-# OPTIONS_GHC -Wno-type-defaults #-}
 module Main (main) where
 
-import HsAoc2021 (someFunc)
+import Data.Text.Read (decimal)
+import HsAoc2021 (day1Part1, day1Part2)
+import Relude
 
 
-main :: IO ()
-main = someFunc
+readInputOfDay1 :: MonadIO m => Text -> m [Int]
+readInputOfDay1 path = do
+  content <- readFileText . toString $ path 
+  pure $ ints content
+  where
+    ints = fmap fst . rights . fmap decimal . words
+
+printResult :: MonadIO m => Int -> Int -> Int -> m()
+printResult day part result =
+  print $ "Day " <> show day <> " / Part " <> show part <> ": " <> show result
+
+main :: (MonadIO m) => m ()
+main = do
+  d1p1 <- runDay1Part1
+  d1p2 <- runDay1Part2
+
+  printResult 1 1 d1p1
+  printResult 1 2 d1p2
+    
+  where
+    inputOfDay1 = readInputOfDay1 "../data/day1/part1/input.txt"
+  
+    runDay1Part1 =
+      day1Part1 <$> inputOfDay1
+
+    runDay1Part2 =
+      day1Part2 <$> inputOfDay1
+
--- hs-aoc2021/src/HsAoc2021.hs
+++ hs-aoc2021/src/HsAoc2021.hs
@@ -1,4 +1,4 @@
-{- |
+{-
 Copyright: (c) 2021 essic
 SPDX-License-Identifier: NONE
 Maintainer: essic <lollancf37@gmail.com>
@@ -7,9 +7,33 @@ Advent of code 2021 in Haskell
 -}
 
 module HsAoc2021
-       ( someFunc
-       ) where
+  ( day1Part1,
+    day1Part2,
+  )
+where
 
+import Relude
+import Safe.Exact (takeExactMay)
 
-someFunc :: IO ()
-someFunc = putStrLn ("someFunc" :: String)
+type Speed = Int
+type Depth = Int
+
+day1Part1 :: [Depth] -> Speed
+day1Part1 [] = 0
+day1Part1 depths@(x : _) =
+  snd $ foldl' speed (x, 0) depths
+  where
+    speed :: (Depth, Speed) -> Depth -> (Depth, Speed)
+    speed (previousDepth, currentSpeed) depth
+      | depth > previousDepth = (depth, currentSpeed + 1)
+      | otherwise = (depth, currentSpeed)
+
+day1Part2 :: [Depth] -> Speed
+day1Part2 depths =
+  execute depths []
+  where
+    execute xs acc =
+      case takeExactMay 3 xs of
+        Nothing -> day1Part1 . reverse $ acc
+        Just xs' -> execute (drop 1 xs) (sum xs' : acc)
+
