daee59f7cf4c39bb587618bbdacf61aea78781e7
essic <lollancf37@gmail.com>
2021-12-05 20:55:46 +0100

    chore: solve day 2

--- hs-aoc2021/app/Main.hs
+++ hs-aoc2021/app/Main.hs
@@ -1,36 +1,80 @@
 {-# OPTIONS_GHC -Wno-type-defaults #-}
+
 module Main (main) where
 
 import Data.Text.Read (decimal)
-import HsAoc2021 (day1Part1, day1Part2)
+import qualified Data.Set as Set (fromList)
+import qualified HsAoc2021  as Aoc (day1Part1, day1Part2, day2Part1, day2Part2, Command(..), Direction (..) )
 import Relude
+import qualified Text.Megaparsec as TM
+import qualified Text.Megaparsec.Char as TMC
+import qualified Text.Megaparsec.Char.Lexer as L
+import Data.Char (isLetter)
 
+main :: (MonadIO m) => m ()
+main = do
+  runDay1 1 Aoc.day1Part1
+  runDay1 2 Aoc.day1Part2
 
-readInputOfDay1 :: MonadIO m => Text -> m [Int]
-readInputOfDay1 path = do
-  content <- readFileText . toString $ path 
-  pure $ ints content
+  runDay2 1 Aoc.day2Part1
+  runDay2 2 Aoc.day2Part2
+
   where
-    ints = fmap fst . rights . fmap decimal . words
+    runDay2 :: MonadIO m => Int -> ([Aoc.Command] -> Int) -> m ()
+    runDay2 part runDay2Func = do
+      input <- readInputOfDay2 "../data/day2/part1/input.txt"
+      case input of
+          Left _ -> print $ "Failure to parse input of Day 2 / Part " <> show part
+          Right cmds -> printResult 2 part $ runDay2Func cmds
 
+    runDay1 :: MonadIO m => Int -> ([Int] -> Int) -> m () 
+    runDay1 part f = do
+      input <- readInputOfDay1 "../data/day1/part1/input.txt"
+      printResult 1 part $ f input
+
+-- Dumb function to print result
 printResult :: MonadIO m => Int -> Int -> Int -> m()
 printResult day part result =
   print $ "Day " <> show day <> " / Part " <> show part <> ": " <> show result
 
-main :: (MonadIO m) => m ()
-main = do
-  d1p1 <- runDay1Part1
-  d1p2 <- runDay1Part2
-
-  printResult 1 1 d1p1
-  printResult 1 2 d1p2
-    
+-- Getting input for day 1
+readInputOfDay1 :: MonadIO m => Text -> m [Int]
+readInputOfDay1 path = do
+  content <- readFileText . toString $ path
+  pure $ ints content
   where
-    inputOfDay1 = readInputOfDay1 "../data/day1/part1/input.txt"
-  
-    runDay1Part1 =
-      day1Part1 <$> inputOfDay1
+    ints = fmap fst . rights . fmap decimal . words
 
-    runDay1Part2 =
-      day1Part2 <$> inputOfDay1
+-- Getting input for day 2
+readInputOfDay2 :: MonadIO m => Text -> m (Either (TM.ParseErrorBundle Text Void) [Aoc.Command])
+readInputOfDay2 path = do
+  content <- readFile . toString $ path
+  TM.runParserT parseCommands "input" . toText $ content
+
+
+type Day2InputParserT = TM.ParsecT Void Text
+
+parseCommands :: Day2InputParserT m [Aoc.Command]
+parseCommands = do
+  TM.sepEndBy parseCommand TMC.space
+
+parseCommand :: Day2InputParserT m Aoc.Command
+parseCommand = do
+  direction <- parseDirection
+  speed <- L.decimal
+  return $ Aoc.Command { Aoc.speed = speed , Aoc.direction = direction }
+
+parseDirection :: TM.ParsecT Void Text m Aoc.Direction
+parseDirection = do
+  rawDirection <- TM.many (TM.satisfy isLetter :: Day2InputParserT m Char)
+  _ <- TMC.space
+  toDirection . toText $ rawDirection
+  where
+    toDirection x =
+      case x of
+        "forward" -> return Aoc.Forward
+        "down" -> return Aoc.Down
+        "up" -> return Aoc.Up
+        _ -> TM.failure Nothing (Set.fromList [])
+
 
--- hs-aoc2021/src/HsAoc2021.hs
+++ hs-aoc2021/src/HsAoc2021.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DerivingStrategies #-}
 {-
 Copyright: (c) 2021 essic
 SPDX-License-Identifier: NONE
@@ -9,10 +10,13 @@ Advent of code 2021 in Haskell
 module HsAoc2021
   ( day1Part1,
     day1Part2,
-  )
+    day2Part1,
+    day2Part2,
+    Direction(..),
+    Command(..))
 where
 
-import Relude
+import Relude hiding (Down)
 import Safe.Exact (takeExactMay)
 
 type Speed = Int
@@ -21,12 +25,12 @@ type Depth = Int
 day1Part1 :: [Depth] -> Speed
 day1Part1 [] = 0
 day1Part1 depths@(x : _) =
-  snd $ foldl' speed (x, 0) depths
+  snd $ foldl' computeSpeed (x, 0) depths
   where
-    speed :: (Depth, Speed) -> Depth -> (Depth, Speed)
-    speed (previousDepth, currentSpeed) depth
-      | depth > previousDepth = (depth, currentSpeed + 1)
-      | otherwise = (depth, currentSpeed)
+    computeSpeed :: (Depth, Speed) -> Depth -> (Depth, Speed)
+    computeSpeed (previousDepth, currentSpeed) currentDepth
+      | currentDepth > previousDepth = (currentDepth, currentSpeed + 1)
+      | otherwise = (currentDepth, currentSpeed)
 
 day1Part2 :: [Depth] -> Speed
 day1Part2 depths =
@@ -37,3 +41,53 @@ day1Part2 depths =
         Nothing -> day1Part1 . reverse $ acc
         Just xs' -> execute (drop 1 xs) (sum xs' : acc)
 
+data Direction = Forward | Down | Up
+  deriving stock Show
+
+
+data Command = Command
+  {
+    direction :: Direction
+   ,speed :: Speed
+  }
+  deriving stock Show
+
+data Position = Position 
+  {
+    horizontal :: Int
+   ,depth :: Int
+   ,aim :: Int
+  }
+  deriving stock Show
+
+defaultPosition :: Position
+defaultPosition =
+  Position { horizontal = 0, aim = 0 , depth = 0 }
+
+day2Part1 :: [Command] -> Int
+day2Part1 cmds =
+  execute cmds defaultPosition
+  where
+    execute [] x = horizontal x * depth x
+    execute (y:ys) x = 
+      execute ys newPosition  
+      where
+        newPosition =
+          case direction y of
+            Forward   -> x { horizontal = horizontal x + speed y }
+            Down      -> x { depth = depth x + speed y }
+            Up        -> x { depth = depth x - speed y }
+
+
+day2Part2 :: [Command] -> Int
+day2Part2 cmds =
+  execute cmds defaultPosition
+    where 
+      execute [] x = horizontal x * depth x
+      execute (y:ys) x =
+        let newPosition =
+              case direction y of
+                  Forward   -> x { horizontal = horizontal x + speed y, depth = depth x + (aim x * speed y) }
+                  Down      -> x { aim = aim x + speed y }
+                  Up        -> x { aim = aim x - speed y }
+          in execute ys newPosition
